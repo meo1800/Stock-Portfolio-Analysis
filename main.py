ea18a52a14171f2fd81ef464cadf4482be8b3e40
ahmed elzoghiby <ahmedelzoghiby@MacBook-Air-ahmed.local>
2025-02-27 18:19:57 +0400

    Restructured data_analysis.py, removing code execution. Defined analysis functions for generating weights. Added weighted_cash_investment function to data_analysis. Added visualizations.py for plotting functions

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-#get user inputs
-input_stocks = input(print("Enter stock tickers separated by a comma (ex: AAPL,AMZN,TSLA):"))
+from scripts.data_processing import get_financial_data,pct_daily_return,price_scalling
+
+# Get user inputs for stock tickers, date range, risk-free-rate, initial investment, and simulation runs.
+input_stocks = input('Enter stock tickers separated by a comma (ex: AAPL,AMZN,TSLA):').split(',')
+input_start = input('Enter the start date of the investment in the form "YYYY-MM-DD":')
+input_end = input('Enter the end date for the investment in the form "YYYY-MM-DD:')
+
+# Uses inputs to create a df using yfinance to obtain financial data
+portfolio_close_price_df = get_financial_data(input_stocks,input_start,input_end)
+
+# Scales stock prices to 1
+scaled_portfolio_df = price_scalling(portfolio_close_price_df)
+
+# print(scalled_portfolio_df)
--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -2,27 +2,44 @@ import yfinance as yf
 import pandas as pd 
 import numpy as np
 
-# Function to get financial data of portfolio from inputs using yfinance
-def get_portfolio_data(list, start, end):
+# Get financial data of portfolio from inputs using yfinance
+def get_financial_data(list, start, end):
     df = yf.download(list, start = start, end = end)['Close']
     return df
 
-portfolio_close_price = get_portfolio_data(input_stocks,input_start,input_end)
+# Calculate the percentage daily return of each stock, including all rows and columns
+def pct_daily_return(df):
+    daily_returns = df.iloc[:].pct_change() * 100
+    daily_returns.replace(np.nan,0,inplace = True) # Replaces NaN in first row with zero
+    return daily_returns
 
-# Calculate the percentage daily return of each stock, including all rows and skipping the data column
-stock_daily_returns = portfolio_close_price.iloc[:,1:].pct_change() * 100
+# Scales stock prices to 1
+def price_scaling(raw_prices_df):
+    scaled_prices = raw_prices_df.copy()
+    for i in scaled_prices.columns[:]:
+        scaled_prices[i] = raw_prices_df[i]/raw_prices_df[i].iloc[0]
+    return scaled_prices
 
-# Replacing NaN with zero in the first row
-stock_daily_returns.replace(np.nan,0,inplace = True)
+# Returns a df with the portfolio's weighted cash investment of each stock
+# Adds columns for Total Portfolio Value and Daily Return
+def weighted_cash_investment(raw_prices_df,weights,initial_amount):
 
-# Function to scale stock prices to 1
-def price_scalling(raw_prices_df):
-    scaled_prices_df = raw_prices_df.copy()
-    for i in scaled_prices_df.columns[:]:
-        scaled_prices_df[i] = raw_prices_df[i]/raw_prices_df[i].iloc[0]
-    return scaled_prices_df
+    scaled_prices_df = price_scaling(raw_prices_df)
+    weighted_portfolio_df = pd.DataFrame(index = scaled_prices_df.index)
+    
+    # Obtains stock names along with counter to be used as an index
+    for i, stock in enumerate(scaled_prices_df.columns[1:]):
+        weighted_portfolio_df[stock] = weights[i] * scaled_prices_df[stock]  * initial_amount
+    
+    # Add column (Total Portfolio Value)
+    weighted_portfolio_df['Portfolio Value [$]'] = weighted_portfolio_df.sum(axis = 1, numeric_only = True)
+    
+    # Add column (Portfolio Daily Return) and replace NaNs with zero
+    weighted_portfolio_df['Portfolio Daily Return [%]'] = weighted_portfolio_df['Portfolio Value [$]'].pct_change(1) * 100
+    weighted_portfolio_df.replace(np.nan, 0, inplace = True)
+    
+    return weighted_portfolio_df.round(2)
 
-scalled_portfolio = price_scalling(portfolio_close_price)
 
 
 
