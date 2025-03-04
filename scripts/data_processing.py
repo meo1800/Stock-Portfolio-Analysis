75c29a3e11461064ea54e4b79ed393c14a4dc18c
ahmed elzoghiby <suktort@gmail.com>
2025-03-04 06:55:34 +0400

    Specified close_price_df variable for functions to ensure standard

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -8,23 +8,23 @@ def get_financial_data(list, start, end):
     return df
 
 # Calculate the percentage daily return of each stock, including all rows and columns
-def pct_daily_return(df):
-    daily_returns = df.iloc[:].pct_change() * 100
+def pct_daily_return(close_price_df):
+    daily_returns = close_price_df.iloc[:].pct_change() * 100
     daily_returns.replace(np.nan,0,inplace = True) # Replaces NaN in first row with zero
     return daily_returns
 
 # Scales stock prices to 1
-def price_scalling(raw_prices_df):
-    scaled_prices = raw_prices_df.copy()
+def price_scalling(close_price_df):
+    scaled_prices = close_price_df.copy()
     for i in scaled_prices.columns[:]:
-        scaled_prices[i] = raw_prices_df[i]/raw_prices_df[i].iloc[0]
+        scaled_prices[i] = close_price_df[i]/close_price_df[i].iloc[0]
     return scaled_prices
 
 # Returns a df with the portfolio's weighted cash investment of each stock
 # Adds columns for Total Portfolio Value and Daily Return
-def weighted_cash_investment(raw_prices_df,weights,initial_amount):
+def weighted_cash_investment(close_price_df,weights,initial_amount):
 
-    scaled_prices_df = price_scalling(raw_prices_df)
+    scaled_prices_df = price_scalling(close_price_df)
     weighted_portfolio_df = pd.DataFrame(index = scaled_prices_df.index)
     
     # Obtains stock names along with counter to be used as an index
