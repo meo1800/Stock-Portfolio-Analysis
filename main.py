1ffa41906c94199d4ab2a13795d2fa08adbe0ecb
ahmed elzoghiby <suktort@gmail.com>
2025-03-04 07:01:11 +0400

    Defined placeholders for Monte Carlo simulation input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scripts.data_processing import get_financial_data,pct_daily_return,price_scalling,weighted_cash_investment
 from scripts.visualizations import plot_financial_data
 import streamlit as st
+import numpy as np
 
 # Get user inputs for stock tickers, date range, risk-free-rate, initial investment, and simulation runs.
 input_stocks = input('Enter stock tickers separated by a comma (ex: AAPL,AMZN,TSLA):').split(',')
@@ -19,8 +20,25 @@ scaled_portfolio_df = price_scalling(portfolio_close_price_df)
 # Calculates Daily Return of each stock
 portfolio_daily_returns_df = pct_daily_return(portfolio_close_price_df)
 
+# # Returns asset allocation according to a given weight and initial investment
+# asset_allocation = weighted_cash_investment(portfolio_close_price_df,)
 
-asset_allocation = weighted_cash_investment(portfolio_close_price_df)
+# # Plots a line chart of all stock prices
+# fig_pct_daily_return = plot_financial_data(portfolio_close_price_df,'Portfolio Positions [$]')
 
-# Plots a line chart of all stock prices
-fig_pct_daily_return = plot_financial_data(portfolio_close_price_df,'Portfolio Positions [$]')
+# MONTE CARLO Simulation
+
+# Defining input variables 
+# From user inputs
+sim_runs = input_runs
+initial_investment = input_initial_investment
+n = len(input_stocks)
+
+# Placeholders to store values
+weights_runs = np.zeros((sim_runs, n))
+sharpe_ratio_runs = np.zeros(sim_runs)
+expected_portfolio_returns_runs = np.zeros(sim_runs)
+volatility_runs = np.zeros(sim_runs)
+return_on_investment_runs = np.zeros(sim_runs)
+final_value_runs = np.zeros(sim_runs)
+
